272675e06889704862920fce34960055fa2de041
Sergey <seregaryz@gmail.com>
2018-10-19 18:52:27 +0300

    progress of project19.10.18

--- app/src/main/java/com/example/sereg/taxi/MainActivity.java
+++ app/src/main/java/com/example/sereg/taxi/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.sereg.taxi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                Intent intent = new Intent(this, ActualTrips.class);
+                break;
+            default:
+                break;
+        }
     }
 }
