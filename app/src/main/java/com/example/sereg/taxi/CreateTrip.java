e38a5dd297c7e57bbc5816d1d34a4cf03d94f606
Alexandra Averianova <aleksandra.averyanova@yandex.ru>
2018-10-21 13:22:24 +0300

    Initial

---
+++ app/src/main/java/com/example/sereg/taxi/CreateTrip.java
@@ -0,0 +1,21 @@
+package com.example.sereg.taxi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_trip);
+    }
+
+    public void onButtonClick(View v){
+        EditText name = (EditText)findViewById(R.id.Name);
+        EditText address = (EditText)findViewById(R.id.Address);
+        EditText time = (EditText)findViewById(R.id.Time);
+    }
+}
